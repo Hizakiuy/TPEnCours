c68c69596edcd9b391a861a88f300dac99aa1e5c
Visoiu Mistrih Francis <thegameg1@yahoo.com>
2012-12-15 13:30:09 +0100

    TryParse ajouté à Pretty et Average

--- Average/Average/Program.cs
+++ Average/Average/Program.cs
@@ -10,19 +10,46 @@ namespace Average
         static void Main(string[] args)
         {
             Console.WriteLine("Combien de nombres ?");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Average(n);
+            string nStr = Console.ReadLine(); //input
+            int n = 0; //initialisation pour la fonction TryParse
+            int temp = 0; //pour la fonction TryParse
+
+            if (int.TryParse(nStr, out temp) == false) //verifie si c'est bien un entier
+                Console.Write("Ce n'est pas un entier");
+            else
+            {
+                n = Convert.ToInt32(nStr); //si c'en est un, il le passe en int
+                Average(n); //et applique la fonction sur ce nombre
+            }
+                
+            Console.ReadLine();
+            
         }
 
         static void Average(int n)
         {
-            int somme = 0;
-            for (int i = 0; i <= n; i++)
+            int somme = 0; //initialisation de la somme des nombres à 0 vu que c'est une somme
+            int temp = 0; //pour tryparse
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Nombre " + i);
-                somme += Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nombre " + i); //demande le ieme nombre
+                string newString = Console.ReadLine();
 
+                if (int.TryParse(newString, out temp) == false) //verifie si c'est un entier
+                {
+                    Console.WriteLine(newString + " n'est pas un entier");
+                    i = n; //arrete la boucle
+                    temp += 1; /*je me sers de la variable 'temp' du TryParse pour eviter d'afficher
+                               la moyenne même si j'entre autre chose que des entiers*/
+                }
+                else
+                {
+                    somme += Convert.ToInt32(newString); //ajoute le ieme nombre à la somme des autres
+                    temp = 0; //si la conversion a réussi, je remet temp à 0 pour la condition d'affichage de la moyenne
+                }
             }
+            if (temp == 0) //si aucune exception ne s'est déclanchée, on affiche la moyenne
+                Console.WriteLine("La moyenne est de " + (float)somme / n);
         }
     }
 }
---
+++ PrettyPrintNumbers/PrettyPrintNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrettyPrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int temp; //variable temporaire pour gerer la sortie de la fonction TryParse
+
+            Console.WriteLine("Tu veux partir de quel nombre?");//min
+            string aStr = Console.ReadLine(); //input clavier
+            int a = 0; //variable initialisée à 0 car on ne sait pas si la valeur entrée par l'utilisateur est bien un entier
+
+            Console.WriteLine("Jusqu'au quel?");               //max
+            string bStr = Console.ReadLine();
+            int b = 0;
+
+            Console.WriteLine("Et le pas stp:");               //pas
+            string pStr = Console.ReadLine();
+            int p = 0;
+
+
+            if (int.TryParse(aStr, out temp) == false ||
+                int.TryParse(bStr, out temp) == false ||
+                int.TryParse(pStr, out temp) == false)
+                Console.WriteLine("Je veux des entiers moi. Try again.");
+            else
+            {
+                a = a + Convert.ToInt32(aStr);
+                b = b + Convert.ToInt32(bStr);
+                p = p + Convert.ToInt32(pStr);
+
+
+                if (a >= b) //verification si le deuxieme est plus grand que le premier 
+                    Console.WriteLine("Le premier nombre doit être inferieur au deuxieme");
+                else
+                    PrintNumbers(a, b, p);//applique fonction //ne marche pas si le pas dépasse la derniere valeur...
+            }
+            Console.ReadLine();
+        }
+
+        static void PrintNumbers(int a, int b, int p)
+        {
+            for (int i = a; i <= b; i += p)//boucle commençant à a, jusqu'a b, d'un pas de p
+            {
+                if (i == b) //verifier si i est bien le dernier, pour afficher le "et" avant le dernier nombre
+                    Console.WriteLine(" et " + i);
+                else if (i == b - p) //enlever la virgule avant le "et"
+                    Console.Write(i);
+                else
+                    Console.Write(i + ", "); //les autres cas
+            }
+        }
+    }
+}
