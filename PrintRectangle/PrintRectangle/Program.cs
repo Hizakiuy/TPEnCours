a4f2edd7d12db4309a529fb58cd0a48144aee5fd
Visoiu Mistrih Francis <thegameg1@yahoo.com>
2012-12-15 14:20:09 +0100

    TP Fini

--- PrintRectangle/PrintRectangle/Program.cs
+++ PrintRectangle/PrintRectangle/Program.cs
@@ -9,40 +9,58 @@ namespace PrintRectangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("File moi la largeur batard");
-            int largeur = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("File moi la longueur aussi batard");
-            int longueur = Convert.ToInt32(Console.ReadLine());
-            
-            if (largeur <= 0 || longueur <= 0)
-                Console.WriteLine("Casse toi bollos");
+            int temp = 0; //variable pour le TryParse
+
+            Console.WriteLine("Largeur du rectangle :"); 
+            string largeurStr = Console.ReadLine(); //input
+            int largeur = 0; //initialiser largeur à 0 à cause de TryParse
+
+            if (int.TryParse(largeurStr, out temp) == false) //verifie si c'est bien un entier
+                Console.Write(largeurStr + " n'est pas un entier");
             else
-                PrintSquare(largeur,longueur);
-            
+            {
+                largeur = Convert.ToInt32(largeurStr); //si c'en est un, l'entier largeur passe à la valeur de largeurStr
+
+                Console.WriteLine("File moi la longueur aussi batard");
+                string longueurStr = Console.ReadLine(); //intput
+                int longueur = 0; //initialiser longueur à 0 à cause de TryParse
+
+                if (int.TryParse(longueurStr, out temp) == false) //verifie si c'est bien un entier
+                    Console.Write(longueurStr + " n'est pas un entier");
+                else
+                {
+                    longueur = Convert.ToInt32(longueurStr); //si c'en est un, l'entier longueur passe à la valeur de largeurStr
+
+                    if (largeur <= 0 || longueur <= 0) //verifie si les valeurs sont positives ou 0
+                        Console.WriteLine("Je veux des valeurs positives quand même hein.");
+                    else
+                        PrintSquare(largeur, longueur); //applique la fonction
+                }
+            }
             Console.ReadLine();
         }
 
         static void PrintSquare(int largeur, int longueur)
         {
-            string line = "";
-            string etoiles = "";
+            string line = ""; //string utilisée pour accumuler les espaces à l'interieur du rectangle
+            string etoiles = ""; //string utilisée pour accumuler les etoiles du bord d'en haut et d'en bas du rectangle
 
-                for (int i = 2; i < largeur; i++)
-                {
-                    line = line + " ";
-                }
+            for (int i = 2; i < largeur; i++)
+            {
+                line = line + " "; //ajoute un espace à chaque itération, jusqu'à ce que la ligne est ramplie d'espaces
+            }
 
-                for (int k = 0; k < largeur; k++)
-                {
-                    etoiles = etoiles + "*";
-                }
+            for (int k = 0; k < largeur; k++)
+            {
+                etoiles = etoiles + "*"; //ajoute des étoieles pour remplir la ligne de largeur 'largeur'
+            }
 
-                Console.WriteLine(etoiles);
-                for (int j = 2; j < longueur; j++)
-                {
-                Console.WriteLine("*" + line + "*");
-                }
-                Console.WriteLine(etoiles);
+            Console.WriteLine(etoiles); //affiche le côté d'en haut
+            for (int j = 2; j < longueur; j++)
+            {
+                Console.WriteLine("*" + line + "*"); //affiche une ligne vide avec des étoiles sur le côté 'longueur-2' fois
+            }
+            Console.WriteLine(etoiles); //affiche le côté d'en bas
         }
     }
 }
--- Towers/Towers/Program.cs
+++ Towers/Towers/Program.cs
@@ -3,49 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PrintRectangle
+namespace Towers
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Haut de la tour");
-            string haut = Console.ReadLine();
+            string haut = Console.ReadLine();//input haut
 
             Console.WriteLine("Etage decor");
-            string decor = Console.ReadLine();
+            string decor = Console.ReadLine();//input decor
 
             Console.WriteLine("Etage vide");
-            string vide = Console.ReadLine();
+            string vide = Console.ReadLine();//input vide
 
             Console.WriteLine("Bas de la tour");
-            string bas = Console.ReadLine();
+            string bas = Console.ReadLine();//input bas
 
             Console.WriteLine("Combien d'etages");
-            int etages = Convert.ToInt32(Console.ReadLine());
-            
-            if (etages < 0)
-                Console.WriteLine("Etage negatif...t'es con ou quoi?");
+            int temp = 0; //variable pour TryParse
+            int etages = 0; //initialisée à 0 pour TryParse
+            string etagesStr = Console.ReadLine(); //input nr. d'étages
+
+            if (int.TryParse(etagesStr, out temp) == false) //verifie si c'est bien un entier
+                Console.Write(etagesStr + " n'est pas un entier");
             else
-                Towers(etages, haut, decor, vide, bas);
+            {
+                etages = Convert.ToInt32(etagesStr); //si c'en est un, l'entier etages passe à la valeur de etagesStr
+
+                if (etages < 0) //verifie si le nombre d'étages n'est pas negatif
+                    Console.WriteLine("Etage negatif...t'es con ou quoi? Casse toi!");
+                else
+                    Towers(etages, haut, decor, vide, bas); //applique la fonction
+            }
 
             Console.ReadLine();
         }
 
         static void Towers(int etages, string haut, string decor, string vide, string bas)
         {
-            Console.WriteLine("Tiens ta tour wesh");
+            Console.WriteLine();
+            Console.WriteLine("Tiens ta tour");
             Console.WriteLine();
 
-            Console.WriteLine(haut);
-            
-            for (int j = 0; j < etages; j++)
+            Console.WriteLine(haut); //affiche le haut
+
+            for (int j = 0; j < etages; j++) //bouche pour afficher l'interieur de la tour
             {
-                Console.WriteLine(vide);
-                Console.WriteLine(decor);
+                Console.WriteLine(vide); //affiche l'étage vide
+                Console.WriteLine(decor); //affiche l'étage décoré
             }
-            Console.WriteLine(vide);
-            Console.WriteLine(bas);
+            Console.WriteLine(vide); //affiche un étage vide avant le bas
+            Console.WriteLine(bas); //affiche le bas
         }
     }
 }
